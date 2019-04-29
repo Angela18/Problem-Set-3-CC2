f506b37ca8c0c2b24ecbf2b43e73360206f18380
Angela Aquise Santos <Angela18@users.noreply.github.com>
2019-04-29 01:07:20 -0500

    5. Polygon

---
+++ poly_geometry.cpp
@@ -0,0 +1,11 @@
+#include"poly_geometry.h"
+
+int Polygon::n=0;
+
+Polygon::Polygon(const PointArray &pa): points(pa){
+    ++numPolygons;
+}
+
+Polygon::Polygon(const Point pointArr[],const int numPoints):points(pointArr,numPoints){
+    ++numPolygons;
+}
